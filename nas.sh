5775d6695f1b8ca17715efc1ad3d2c72ab79925d
Rolf Kaiser <1090569+rkaiser0324@users.noreply.github.com>
2019-12-10 08:37:46 -0800

    fix: skip trying to change the IP each time

--- nas.sh
+++ nas.sh
@@ -1,26 +1,24 @@
 #!/bin/sh
 #
-# This script executes commands on the NAS, and requires that
-# you configure the two details:
+# This script executes commands on the NAS, after you configure the IP, admin password, and MAC address of the NAS.
 #
-#  1.  The IP address of the NAS
-#
-#  2.  The password for the "admin" user.
-#
-#  3.  Uncomment (#) and change the MAC of the NAS. (Possibly just change the last 3 bytes)
-#
+# Syntax:
+#	./nas.sh <command>
+#	./nas.sh ls -l
 
-
+# Set the IP address of the NAS
 IP=192.168.1.127
 
+# Set the password for the "admin" user on the web UI
 ADMIN_PASS=password
 
+# Set, and if necessary, change the MAC of the NAS (may only need to change the last 3 bytes)
 MAC=cc:e1:d5:a6:0d:5d
 
 if [ -n "$MAC" ]; then
-	java -jar acp_commander.jar  -q -t ${IP} -ip ${IP} -m ${MAC} -pw ${ADMIN_PASS} -c "$*"
+	java -jar acp_commander.jar  -q -t ${IP} -m ${MAC} -pw ${ADMIN_PASS} -c "$*"
 else
-	echo NOTE: MAC not defined. If this doesn't work try to define your NAS' MAC address above.
-	java -jar acp_commander.jar  -q -t ${IP} -ip ${IP} -pw ${ADMIN_PASS} -c "$*"
+	echo "NOTE: MAC not defined. If this doesn't work try to define your NAS' MAC address above."
+	java -jar acp_commander.jar  -q -t ${IP} -pw ${ADMIN_PASS} -c "$*"
 fi
 
